2df0abcdd566e25c2e359f266b550704a5f5cdc4
Minh Ta <minh.ta@creativeintersection.com>
2023-12-30 13:28:27 +1000

    * Update font-family * Minor update for Footer

--- src/features/Footer.tsx
+++ src/features/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer(props: any) {
   return (
     <div>
       <p className="text-center fw-bold mt-3 mb-0">
-        <span className="text-multi-color fst-italic">Developed by </span>
+        <span className="text-multi-color">Developed by </span>
         Minh Ta
       </p>
     </div>
